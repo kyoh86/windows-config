f6616abe37ee31b370729ab95bbf8a8b19562931
kyoh86 <me@kyoh86.dev>
2023-02-26 23:24:36 +0900

    first wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,4 +2,13 @@ local wezterm = require("wezterm")
 return {
   font = wezterm.font("PlemolJP Console NF"),
   default_prog = { "wsl.exe", "--distribution", "Ubuntu", "--cd", "~", "--exec", "/bin/zsh", "-l", "-c", "nvim" },
+  color_scheme_dirs = { "~\\Projects\\github.com\\kyoh86" },
+  color_scheme = "momiji",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  hide_tab_bar_if_only_one_tab = true,
 }
