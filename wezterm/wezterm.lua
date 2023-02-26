b78b2dbc2e3cfd1cd0b9a9b949ddf23841ccaeac
kyoh86 <me@kyoh86.dev>
2023-02-26 23:39:55 +0900

    fix wezterm colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require("wezterm")
 return {
   font = wezterm.font("PlemolJP Console NF"),
   default_prog = { "wsl.exe", "--distribution", "Ubuntu", "--cd", "~", "--exec", "/bin/zsh", "-l", "-c", "nvim" },
-  color_scheme_dirs = { "~\\Projects\\github.com\\kyoh86" },
   color_scheme = "momiji",
   window_padding = {
     left = 0,
