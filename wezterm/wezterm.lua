3ee869a101baf8afc90a2df3567dd1576aa5eda4
kyoh86 <me@kyoh86.dev>
2023-05-27 15:57:12 +0900

    Run Neovim on WSL as domain

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,34 +1,63 @@
 local wezterm = require("wezterm")
 
-local default_prog = { "wsl.exe", "--distribution", "Ubuntu", "--cd", "~", "--exec", "/bin/zsh", "-l", "-c", "nvim" }
-return {
+---@class Named
+---@field name string
+local _ = nil
+---@class WSLDomain : Named
+---@field distribution string
+local _ = nil
+
+--- Merge two tables
+---@generic T : table
+---@param t1 T
+---@param t2 T
+---@return T
+local function tbl_merge(t1, t2)
+  local merged = {}
+  for _, t in pairs({ t1, t2 }) do
+    for k, v in pairs(t) do
+      merged[k] = v
+    end
+  end
+  return merged
+end
+
+---@generic T : Named
+---@param name string A name to find it out
+---@param list T[]
+---@return T
+local function find_name(name, list)
+  for _, dom in ipairs(list) do
+    if dom.name == name then
+      return dom
+    end
+  end
+  return nil
+end
+
+local function build_domains()
+  ---@type WSLDomain[]
+  local wsl_domains = wezterm.default_wsl_domains()
+
+  local ubuntu = find_name("WSL:Ubuntu", wsl_domains)
+  if ubuntu == nil then
+    return {}
+  end
+
+  table.insert(wsl_domains, tbl_merge(ubuntu, { name = "WSL:Neovim", default_prog = { "/bin/zsh", "-l", "-c", "nvim" } }))
+  table.insert(wsl_domains, tbl_merge(ubuntu, { name = "WSL:Neovim-test", default_prog = { "/bin/zsh", "-l", "-c", "NVIM_APPNAME=nvim-test nvim" } }))
+
+  return { wsl_domains = wsl_domains, default_domain = "WSL:Neovim" }
+end
+
+return tbl_merge(build_domains(), {
   font = wezterm.font_with_fallback({ "PlemolJP Console", "Symbols NF 1000-em" }),
-  default_prog = default_prog,
-  launch_menu = {
-    {
-      label = "Neovim",
-      args = default_prog,
-    },
-    {
-      label = "Neovim for test",
-      args = { "wsl.exe", "--distribution", "Ubuntu", "--cd", "~", "--exec", "/bin/zsh", "-l", "-c", "NVIM_APPNAME=nvim-test nvim" },
-    },
-  },
   color_scheme = "momiji",
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
   keys = {
-    {
-      key = "t",
-      mods = "ALT",
-      action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|LAUNCH_MENU_ITEMS" }),
-    },
+    { key = "p", mods = "ALT", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|DOMAINS" }) },
   },
   front_end = "WebGpu",
-  -- hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
-}
+})
