4a7988dc634d7ef9f2bf093e5643ee079e5ef45c
kyoh86 <me@kyoh86.dev>
2023-06-07 06:53:52 +0900

    fix

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -46,16 +46,18 @@ local function build_domains()
 
   table.insert(wsl_domains, tbl_merge(ubuntu, { name = "WSL:Neovim", default_prog = { "/bin/zsh", "-l", "-c", "nvim" } }))
   table.insert(wsl_domains, tbl_merge(ubuntu, { name = "WSL:Neovim-test", default_prog = { "/bin/zsh", "-l", "-c", "NVIM_APPNAME=nvim-test nvim" }, default_cwd = "~/Projects/github.com/kyoh86/dotfiles" }))
+  table.insert(wsl_domains, tbl_merge(ubuntu, { name = "WSL:Vim", default_prog = { "/bin/zsh", "-l", "-c", "vim" } }))
 
   return { wsl_domains = wsl_domains, default_domain = "WSL:Neovim" }
 end
 
 return tbl_merge(build_domains(), {
-  font = wezterm.font_with_fallback({ "PlemolJP Console", "Symbols NF 1000-em" }),
+  font = wezterm.font_with_fallback({ "PlemolJP Console NF" }),
   color_scheme = "momiji",
   hide_tab_bar_if_only_one_tab = true,
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
   keys = {
+    { key = "n", mods = "ALT", action = wezterm.action.SpawnWindow },
     { key = "l", mods = "ALT", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|DOMAINS" }) },
     { key = "p", mods = "ALT", action = wezterm.action.ActivateCommandPalette },
     { key = "t", mods = "SHIFT|ALT", action = wezterm.action.SpawnCommandInNewWindow({ label = "Start new nvim-test", args = {}, domain = { DomainName = "WSL:Neovim-test" } }) },
