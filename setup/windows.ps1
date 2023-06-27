bc3275f53a1ebc1d8883d6a4e80c148668f2b01e
kyoh86 <me@kyoh86.dev>
2023-06-27 09:52:00 +0900

    add setup script

---
+++ setup/windows.ps1
@@ -0,0 +1,29 @@
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+iwr -useb get.scoop.sh | iex
+scoop install sudo
+scoop install win32yank
+
+winget install 7zip.7zip
+winget install AgileBits.1Password
+winget install Discord.Discord
+winget install Git.Git
+winget install Google.Chrome
+winget install Google.Drive
+winget install JannisX11.Blockbench
+winget install Microsoft.PowerShell
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.WindowsTerminal
+winget install Nota.Gyazo
+winget install OBSProject.OBSStudio
+winget install RARLab.WinRAR
+winget install SlackTechnologies.Slack
+winget install Valve.Steam
+winget install Xmind.Xmind
+winget install inkdrop
+winget install wez.wezterm
+winget upgrade --all
+Set-content -Path ~/wslconfig -Value "[wsl2]","localhostForwarding = true","memory = 16GB","[interop]","appendWindowsPath = false"
+
+# Import .config/WindowsTerminal/settings.json
+
